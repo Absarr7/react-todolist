b0cbdf663e8cf6f4fbd839fc95bf3b30ae0eb76c
Absar Ahmad <absar4060@gmail.com>
2023-04-09 16:26:30 +0530

    donne

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = ()=>{
   return (
         <nav className="navbar navbar-expand-lg naaav navbar-dark">
       <div className="container-fluid">
-        <h1 className="navbar-brand text-white fs-1 fw-bold mx-3" to="/">ABSAR</h1>
+        <a className="navbar-brand text-white fs-3 fw-bold mx-3" href="#">ABSAR</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
